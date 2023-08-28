36d8fdac91996abc3a9b98b56f7852b65dca4103
dmitry <vuylov@e-queo.com>
2023-08-28 13:57:51 +0300

    Fix HEALTH_CHECK_IO_UID value

--- src/Health/HealthCheckFactory.php
+++ src/Health/HealthCheckFactory.php
@@ -10,7 +10,7 @@ class HealthCheckFactory
 {
     public static function build(array $env): HealthChecker
     {
-        if (isset($env['HEALTH_CHECK_IO_UID'])) {
+        if (isset($env['HEALTH_CHECK_IO_UID']) && mb_strlen($env['HEALTH_CHECK_IO_UID']) > 0) {
 
             $config = new HealthcheckIOConfig($env['HEALTH_CHECK_IO_UID']);
             if (isset($env['HEALTH_CHECK_IO_HOST']) && mb_strlen($env['HEALTH_CHECK_IO_HOST']) > 0) {
