060e3d1c424a9d91abe33f6f69762ad1ded7deec
IvanHunters <xorokhotnikov@gmail.com>
2026-06-25 23:42:04 +0300

    feat(health): передаём тело сообщения в пинги HealthChecker success и failure
    Signed-off-by: IvanHunters <xorokhotnikov@gmail.com>

--- src/Health/NullHealthChecker.php
+++ src/Health/NullHealthChecker.php
@@ -8,12 +8,12 @@ namespace Ekvio\Integration\Skeleton\Health;
 
 class NullHealthChecker implements HealthChecker
 {
-    public function success(): void
+    public function success(string $body = ''): void
     {
         //do nothing
     }
 
-    public function failure(): void
+    public function failure(string $body = ''): void
     {
         //do nothing
     }
